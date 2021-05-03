b63e82231bee1a3af608583174d0ef37751ecf4d
Atharva-Sawant-Pro <78305129+Atharva-Sawant-Pro@users.noreply.github.com>
2021-05-03 16:51:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,5 +3,18 @@ function setup(){
     canvas.position(750,150);
     video=createCapture(VIDEO);
     video.size(550,750);
-    video.position(150,50)
+    video.position(150,50);
+    poseNet=ml5.poseNet(video,modalLoaded);
+    poseNet=poseNet.on('pose',gotPoses);
+}
+function modalLoaded(){
+    console.log("Poos naat is Inisoolized ");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+function draw(){
+    background("#808080");
 }
